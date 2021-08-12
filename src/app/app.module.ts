45fda3d1236c5f671d8e1b7749551031620bcbdb
Nemanja Rajkovic <clartred@gmail.com>
2021-08-12 21:17:27 +0200

    stakurac

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,30 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { Game1Component } from './game1/game1.component';
 import { HomeComponent } from './home/home.component';
-import { HomeMissclickComponent } from './home-missclick/home-missclick.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-import { YouDiedComponent } from './game1/you-died/you-died.component';
-import { Game2Component } from './game2/game2.component';
-import { CardComponent } from './game2/card/card.component';
+import { MatButtonModule } from '@angular/material/button';
+import { DialogComponent } from './dialog/dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Game1Component,
     HomeComponent,
-    HomeMissclickComponent,
-    YouDiedComponent,
-    Game2Component,
-    CardComponent
+    DialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+
+import { DialogComponent } from '../dialog/dialog.component';
 
 @Component({
   selector: 'app-home',
@@ -9,23 +12,62 @@ import { Router } from '@angular/router';
 export class HomeComponent implements OnInit {
 
   constructor(
-    private router: Router
+    private router: Router,
+    public dialog: MatDialog,
   ) { }
+
+  leNumber = 1;
 
   ngOnInit(): void {
     document.body.style.background = "#cccccc";
+
+
+    // this.openDialog('Ime mora biti duze od 6 karaktera.', '350px', '300px', false);
+    this.openDialogs(this.leNumber)
   }
 
-  accept(){
+  openDialogs(number) {
+    if (number === 1) {
+      this.openDialog('nemam nista pametno da kazem.', '350px', '300px', false);
+    } else if (number === 2) {
+      this.openDialog('osim da te mnogo volim.', '350px', '300px', false);
+    } else if (number === 3) {
+      this.openDialog('i da jedva cekam da se vratis...', '350px', '300px', false);
+    } else if (number === 4) {
+      this.openDialog('...da zajedno budemo sami', '350px', '300px', false);
+    } else if (number === 5) {
+      this.openDialog('Mora biti da sam glup?', '350px', '300px', true);
+    }
+  }
+
+  accept() {
     this.router.navigate(['kjzdfn;an;rdgoaerihgaerhglaeruiagergilrdf']);
   }
 
-  decline(){
+  decline() {
     window.location.href = "https://youtu.be/dQw4w9WgXcQ";
   }
 
-  missclick(){
+  missclick() {
     this.router.navigate(['home-missclick']);
+  }
+
+  openDialog(text: string, height: string, width: string, action: boolean) {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      width: width,
+      height: height,
+      data: text
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.leNumber++;
+
+      if (action) {
+        window.location.href = "https://youtu.be/dQw4w9WgXcQ";
+      } else {
+        this.openDialogs(this.leNumber)
+      }
+    });
   }
 
 }
